1d961c744106b97c75a29077b6939f24a72c3edd
shradha <shradhagowda093@gmail.com>
2025-08-07 21:01:37 +0530

    Add recursion8.java for reverse string using recursion

---
+++ practice/recursion8.java
@@ -0,0 +1,15 @@
+public class recursion8 {
+    public static void printRev(String str,int idx){
+        if(idx==0){
+            System.out.print(str.charAt(0));
+return;
+        }
+System.out.print(str.charAt(idx));
+        printRev(str,idx-1);
+    }
+    public static void main(String[]args){
+        String str="sanvi";
+        printRev(str,str.length()-1);
+
+    }
+}
